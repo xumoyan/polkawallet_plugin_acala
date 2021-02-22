6cf898546e54725bf68ab724c65b5c87ca8df1e7
shawn <dhryxy@gmail.com>
2021-02-22 18:45:03 +0800

    refactor i18n files for translation

--- lib/utils/i18n/index.dart
+++ lib/utils/i18n/index.dart
@@ -1,5 +1,7 @@
-import 'package:polkawallet_plugin_acala/utils/i18n/acala.dart';
-import 'package:polkawallet_plugin_acala/utils/i18n/common.dart';
+import 'package:polkawallet_plugin_acala/utils/i18n/en/acala.dart';
+import 'package:polkawallet_plugin_acala/utils/i18n/en/common.dart';
+import 'package:polkawallet_plugin_acala/utils/i18n/zh/acala.dart';
+import 'package:polkawallet_plugin_acala/utils/i18n/zh/common.dart';
 
 const Map<String, Map<String, Map<String, String>>> i18n_full_dic_acala = {
   'en': {'common': enCommon, 'acala': enDex},
